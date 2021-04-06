278a58354bb6332084ae3e5d8d4951e930a53cde
Andres Rondon <amrondonp@gmail.com>
2021-04-05 21:17:12 -0700

    fix

--- libnsga2/evolution.py
+++ libnsga2/evolution.py
@@ -3,12 +3,13 @@ from libnsga2.population import Population
 
 class Evolution:
 
-    def __init__(self, problem, num_of_generations=1000, num_of_individuals=100, num_of_tour_particips=2, tournament_prob=0.9, crossover_param=2, mutation_param=5):
+    def __init__(self, problem, num_of_generations=1000, num_of_individuals=100, num_of_tour_particips=2, tournament_prob=0.9, crossover_param=2, mutation_param=5, show_progress=False):
         self.utils = NSGA2Utils(problem, num_of_individuals, num_of_tour_particips, tournament_prob, crossover_param, mutation_param)
         self.population = None
         self.num_of_generations = num_of_generations
         self.on_generation_finished = []
         self.num_of_individuals = num_of_individuals
+        self.show_progress = show_progress
 
     def evolve(self):
         self.population = self.utils.create_initial_population()
@@ -18,6 +19,9 @@ class Evolution:
         children = self.utils.create_children(self.population)
         returned_population = None
         for i in range(self.num_of_generations):
+            if self.show_progress:
+                print("Generation", i)
+
             self.population.extend(children)
             self.utils.fast_nondominated_sort(self.population)
             new_population = Population()
